a845180715ea3f840c3eb28e400b02fc0633b1e3
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-03-20 20:07:17 +0200

    Implemented StructOpt to parse CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    /// Name of the tmux session
+    #[structopt(short = "sn", long = "session-name")]
+    session_name: String,
+
+    /// Directory to start the session in
+    #[structopt(short = "p", long = "session-path")]
+    path: String,
+
+    /// Number of windows in the session
+    #[structopt(short = "numw", long = "number-windows")]
+    number_windows: u32,
+
+    /// Name of each window
+    #[structopt(short = "wname", long = "window-names")]
+    window_names: Option<Vec<String>>,
+
+    /// Attach or detach the session after creation
+    #[structopt(short, long)]
+    attach: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::from_args();
+    println!("{:?}", opts);
 }
