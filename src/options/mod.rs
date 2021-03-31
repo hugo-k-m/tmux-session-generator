f9e2ac5d928b0b045c8b726d42180c02cec0757f
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-03-31 21:07:33 +0200

    Modified implementation of defaults for CLI options

--- src/options/mod.rs
+++ src/options/mod.rs
@@ -1,89 +1,47 @@
+use std::str::FromStr;
+
 use structopt::StructOpt;
+
+#[derive(Debug)]
+pub struct WindowNames(Vec<String>);
+
+impl FromStr for WindowNames {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let new_s = s.chars().filter(|x| !x.is_whitespace()).collect::<String>();
+        let split = new_s.split(",");
+        let vec = split.map(|x| x.to_string()).collect();
+
+        Ok(WindowNames(vec))
+    }
+}
 
 #[derive(Debug, StructOpt)]
 pub struct Opts {
     /// Name of the tmux session
-    #[structopt(short, long)]
-    pub session_name: Option<String>,
+    #[structopt(short, long, default_value = "new_session")]
+    pub session_name: String,
 
     /// Directory to start the session in
-    #[structopt(short, long)]
-    pub path: Option<String>,
+    #[structopt(short, long, default_value = "~")]
+    pub path: String,
 
     /// Number of windows in the session
-    #[structopt(short, long)]
-    pub number_windows: Option<usize>,
+    #[structopt(short, long, default_value = "3")]
+    pub number_windows: usize,
 
     /// Name of each window
-    #[structopt(short, long)]
-    pub window_names: Option<Vec<String>>,
+    #[structopt(short, long, default_value = "w_1,w_2,w_3")]
+    pub window_names: WindowNames,
 
     /// Attach or detach the session after creation
     #[structopt(short, long)]
-    pub attach: Option<bool>,
-}
-
-impl Default for Opts {
-    fn default() -> Self {
-        let mut w_names = Vec::new();
-        w_names.push("".to_owned());
-        w_names.push("".to_owned());
-        w_names.push("".to_owned());
-
-        Self {
-            session_name: Some("new_session".to_owned()),
-            path: Some("~".to_owned()),
-            number_windows: Some(3),
-            window_names: Some(w_names),
-            attach: Some(false),
-        }
-    }
+    pub attach: bool,
 }
 
 impl Opts {
     pub fn get_opts() -> Opts {
-        let options = Self::from_args();
-
-        if let Opts {
-            session_name: Some(_a),
-            path: Some(_b),
-            number_windows: Some(_c),
-            window_names: Some(_d),
-            attach: Some(_e),
-        } = options
-        {
-            Self::from_args()
-        } else {
-            let default_opts = Self::default();
-
-            let session_name = match options.session_name {
-                Some(i) => Some(i),
-                None => default_opts.session_name,
-            };
-            let path = match options.path {
-                Some(i) => Some(i),
-                None => default_opts.path,
-            };
-            let number_windows = match options.number_windows {
-                Some(i) => Some(i),
-                None => default_opts.number_windows,
-            };
-            let window_names = match options.window_names {
-                Some(i) => Some(i),
-                None => default_opts.window_names,
-            };
-            let attach = match options.attach {
-                Some(i) => Some(i),
-                None => default_opts.attach,
-            };
-
-            Opts {
-                session_name: session_name,
-                path: path,
-                number_windows: number_windows,
-                window_names: window_names,
-                attach: attach,
-            }
-        }
+        Self::from_args()
     }
 }
