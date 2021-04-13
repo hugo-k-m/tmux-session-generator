dc1089db298ddb7e22c82cde7a8110292e16814a
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-04-13 21:37:55 +0200

    Implemented session script methods

---
+++ src/options/sessions.rs
@@ -0,0 +1,63 @@
+use std::fs::File;
+use std::io::Write;
+
+use super::Opts;
+
+impl Opts {
+    pub(super) fn session_script(content: String) -> Result<(), Box<dyn std::error::Error>> {
+        let mut file = File::create("/tmp/session1")?;
+        file.write_all(content.as_bytes())?;
+
+        Ok(())
+    }
+}
+
+impl Opts {
+    pub(super) fn session_script_contents(
+        command: &String,
+        detach: &bool,
+        name_window: &Option<String>,
+        session_name: &String,
+        target_session: &Option<String>,
+        x: &Option<usize>,
+        y: &Option<usize>,
+    ) -> String {
+        let mut content = "#!/bin/sh\n\n".to_owned();
+
+        let det = if detach.to_owned() { "-d" } else { "" };
+
+        let name_w = if let Some(nw) = name_window {
+            format!("-n ${}", nw)
+        } else {
+            "".to_string()
+        };
+
+        let target_s = if let Some(ts) = target_session {
+            format!("-t ${}", ts)
+        } else {
+            "".to_string()
+        };
+
+        let width = if let Some(w) = x {
+            format!("-x ${}", w)
+        } else {
+            "".to_string()
+        };
+
+        let height = if let Some(h) = y {
+            format!("-y ${}", h)
+        } else {
+            "".to_string()
+        };
+
+        content.push_str(&format!("session='{:?}'\n", session_name));
+        content.push_str(&format!("session_path='{:?}'\n", command));
+
+        content.push_str(&format!(
+            "tmux new-session {} -s ${} -c ${} {} {} {} {}",
+            det, session_name, command, name_w, target_s, width, height
+        ));
+
+        content
+    }
+}
