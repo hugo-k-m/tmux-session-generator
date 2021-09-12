86547c02332a423c9d2c72df069f4c5f0442ff97
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-09-12 20:13:47 +0200

    Modified directory creation function
    The function returns an error if a directory other than the tmuxsg home
    directory already exists.

--- lib/src/dir.rs
+++ lib/src/dir.rs
@@ -1,12 +1,15 @@
 //! Directory helper methods
 
+use crate::{err::DirectoryError, produce_directory_error};
 use std::{fs, path::PathBuf};
 
 pub fn create_dir(parent: PathBuf, child: String) -> Result<PathBuf, Box<dyn std::error::Error>> {
-    let new_dir = parent.join(child);
+    let new_dir = parent.join(&child);
 
     if !new_dir.is_dir() {
         fs::create_dir(&new_dir)?;
+    } else if child != ".tmuxsg" {
+        produce_directory_error!(child);
     }
 
     Ok(new_dir)
