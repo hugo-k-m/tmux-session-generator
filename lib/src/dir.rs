efbffa9ac725031edaaaa4709ae037afd3272b68
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2021-09-20 22:15:39 +0200

    Directory module
    * Modified the directory module error message
    * Modified the module to return the custom result

--- lib/src/dir.rs
+++ lib/src/dir.rs
@@ -1,15 +1,18 @@
 //! Directory helper methods
 
-use crate::{err::DirectoryError, produce_directory_error};
+use crate::{
+    err::{CustomResult, DirectoryError},
+    produce_directory_error,
+};
 use std::{fs, path::PathBuf};
 
-pub fn create_dir(parent: PathBuf, child: String) -> Result<PathBuf, Box<dyn std::error::Error>> {
+pub fn create_dir(parent: PathBuf, child: String) -> CustomResult<PathBuf> {
     let new_dir = parent.join(&child);
 
     if !new_dir.is_dir() {
         fs::create_dir(&new_dir)?;
     } else if child != ".tmuxsg" {
-        produce_directory_error!(child);
+        produce_directory_error!(child + " session already exists!");
     }
 
     Ok(new_dir)
